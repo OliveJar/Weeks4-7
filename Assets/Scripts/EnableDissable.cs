9788be4cf2f3781213c34b0578fa2b7b2b070828
cruzen <cruzen@OA-S141-26>
2025-01-28 16:50:39 -0500

    Scene and script update (turning on and off)

--- Assets/Scripts/EnableDissable.cs
+++ Assets/Scripts/EnableDissable.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnableDissable : MonoBehaviour
 {
+    public SpriteRenderer sr;
+    public GameObject go;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class EnableDissable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            sr.enabled = false;
+            go.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sr.enabled = true;
+            go.SetActive(true);
+
+            
+        }
     }
 }
