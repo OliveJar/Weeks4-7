cad4a99f2a82883bd267e791c419b1ea28e516de
OliveJar <encru1010@gmail.com>
2025-02-01 22:45:23 -0500

    Scene and script update

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public Transform player;
+
     private float t;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,11 +18,11 @@ public class Camera : MonoBehaviour
     {
         t += 0.5f * Time.deltaTime;
 
-        if(t>=1)
+        if (t >= 1)
         {
             t = 0;
         }
 
-        transform.position = new Vector3 (Mathf.Lerp(player.transform.position.x, player.transform.position.x, t), 0, -1);
+        transform.position = new Vector3(Mathf.Lerp(player.transform.position.x, player.transform.position.x, t), 0, -1);
     }
 }
--- Assets/Scripts/Player movement.cs
+++ Assets/Scripts/Player movement.cs
@@ -6,6 +6,9 @@ public class Playermovement : MonoBehaviour
 {
     public Rigidbody2D rb;
 
+    public Transform Weapon;
+    private float y;
+    private float x;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +19,18 @@ public class Playermovement : MonoBehaviour
     void Update()
     {
 
-        rb.velocity = new Vector2 (Input.GetAxis("Horizontal") * 10, rb.velocity.y);
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * 10, rb.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
         {
             rb.AddForce(Vector2.up * 8, ForceMode2D.Impulse);
+        }
+
+        x = Weapon.transform.position.x - transform.position.x;
+        y = Weapon.transform.position.y - transform.position.y;
+        if (x < 1.5 && y < 1.5)
+        {
+            Weapon.transform.position = transform.position;
         }
     }
 
