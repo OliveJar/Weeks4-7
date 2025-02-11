fcb1a40a6c444daca116f25e7f0048f42f0f8f1c
cruzen <cruzen@OA-S141-15>
2025-02-11 18:05:36 -0500

    finished ammo pickup noise and created enemy with bullet path "raycast"

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -7,18 +7,30 @@ public class Ammo : MonoBehaviour
     public GameObject ammo;
     public AudioClip ammoclip;
     public AudioSource ammoSound;
+    public float time = 5;
+    private float T = 300;
     private float x;
     private float y;
 
     // Update is called once per frame
     void Update()
     {
+        T += time * Time.deltaTime;
+
+
         x = ammo.transform.position.x - transform.position.x;
         y = ammo.transform.position.y - transform.position.y;
 
-        if (x < 1.5 && y < 1.5)
+        if (x < 1.5 && y < 1.5 && !ammoSound.isPlaying)
         {
             ammoSound.PlayOneShot(ammoclip);
+        }
+        if (ammoSound.isPlaying)
+        {
+            T--;
+        }
+        if (T <= 0)
+        {
             ammo.SetActive(false);
         }
     }
